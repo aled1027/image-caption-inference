cfc5d91881a7e848017cacb3931b5da66d539ede
djungelorm <djungelorm@users.noreply.github.com>
2016-07-27 16:20:33 -0700

    Add image-caption lein project

---
+++ image-caption/test/image_caption/core_test.clj
@@ -0,0 +1,7 @@
+(ns image-caption.core-test
+  (:require [clojure.test :refer :all]
+            [image-caption.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
