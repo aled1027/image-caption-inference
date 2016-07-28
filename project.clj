7a51f09499f03101a70c4bbb7ef1e522a34c80d6
Timon Gehr <timon.gehr@gmx.ch>
2016-07-28 02:00:22 +0200

    Merge subproject.

--- project.clj
+++ project.clj
@@ -1,19 +1,9 @@
-(defproject exercises "0.1.0-SNAPSHOT"
-  :description "Source code and exercise worksheets for the PPAML summer school on Anglican"
-  :url "https://bitbucket.org/probprog/ppaml-summer-school-2016/"
+(defproject image-caption "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-gorilla "0.3.4"]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [anglican "1.0.0"]
-                 [org.nfrac/cljbox2d.testbed "0.5.0"]
-                 [org.nfrac/cljbox2d "0.5.0"]
-                 [org.clojure/data.priority-map "0.0.7"]
-                 [net.mikera/core.matrix "0.52.2"]
-                 [net.mikera/vectorz-clj "0.44.1"]
-                 [net.polyc0l0r/clj-hdf5 "0.2.2-SNAPSHOT"]]
-  :java-source-paths ["src/ox_captcha"]
-  :main ^:skip-aot examples.core
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot image-caption.core
   :target-path "target/%s"
-  :jvm-opts ["-Xmx4g" "-Xms2g"]
   :profiles {:uberjar {:aot :all}})
