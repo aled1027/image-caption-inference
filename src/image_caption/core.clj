160a23624fbd0eecdd1ad880f801b8e2c27ec679
Alex Ledger <a.led1027@gmail.com>
2016-07-29 10:50:36 -0700

    small change to post processing

--- src/image_caption/core.clj
+++ src/image_caption/core.clj
@@ -1,20 +1,23 @@
 (ns image_caption.core
   (:use [image_caption globals images sentences images_renderer images_similarity])
   (:use [anglican.core :exclude [-main]])
-  (:use [anglican runtime emit])
+  (:use [anglican runtime emit inference])
   (:gen-class))
 
 (defn -main
   [& args]
   (let [image (read-image "input.png")
-        samples (take 10 (doquery :smc generate-image [image] :numer-of-particles 1000))]
-    (println (first samples))
-    (save-many-images (map #(:image (:result %)) samples) "output/image")
-    (println (map
-               #(image-distance
-                 (get-greyscale-pixels image)
-                 (get-greyscale-pixels (:image (:result %))))
-              samples))))
+        samples (take 10 (doquery :smc generate-image [image] :numer-of-particles 100))
+        samples-2 (collect-results samples)]
+    (println (first samples-2))))
+      
+
+    ;(save-many-images (map #(:image (:result %)) samples) "output/image")
+    ;(println (map
+    ;           #(image-distance
+    ;             (get-greyscale-pixels image)
+    ;             (get-greyscale-pixels (:image (:result %))))
+    ;          samples))))
 
 ;(defn -main
 ;  [& args]
